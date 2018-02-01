031c7a3e150d5e8fdd04a8cdb80a97729a6cbadf
root <root@DESKTOP-QR09MSN.localdomain>
2018-02-01 22:17:31 +0800

    脚本纠错

--- rebuild.sh
+++ rebuild.sh
@@ -2,7 +2,15 @@
 
 sudo apt install nodejs-legacy
 sudo apt install npm
-npm install hexo --save
-hexo init
-npm install
+
+npm install hexo-server --save
+npm install hexo-admin --save
+npm install hexo-generator-archive --save
+npm install hexo-generator-feed --save
+npm install hexo-generator-search --save
+npm install hexo-generator-tag --save
 npm install hexo-deployer-git --save
+npm install hexo-generator-sitemap --save
+npm install hexo-deployer-git
+
+npm install
--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 if [ x$1 != x ]
 then
     git commit -m $1
-    git push origin master:master
+    git push origin
     hexo d -g
 else
     echo "error"
