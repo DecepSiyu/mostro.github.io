467d18e8bd52f75016822e62238acd3d92c8c808
root <root@DESKTOP-QR09MSN.localdomain>
2018-02-01 22:58:25 +0800

    再再次修改脚本

--- rebuild.sh
+++ rebuild.sh
@@ -12,5 +12,10 @@ npm install hexo-generator-tag --save
 npm install hexo-deployer-git --save
 npm install hexo-generator-sitemap --save
 npm install hexo-deployer-git
+npm install hexo-generator-category --save
+npm install hexo-generator-index --save
+npm install hexo-renderer-ejs --save
+npm install hexo-renderer-marked --save
+npm install hexo-renderer-stylus --save
 
 npm install
--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 if [ x$1 != x ]
 then
+    hexo d -g
     git commit -m $1
     git push origin
-    hexo d -g
 else
     echo "error"
 fi
