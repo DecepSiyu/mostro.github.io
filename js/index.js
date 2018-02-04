8d69dfbdcdaed530566f4246f35033981be586ee
root <root@DESKTOP-QR09MSN.localdomain>
2018-02-04 19:59:43 +0800

    Site updated: 2018-02-04 19:59:41

---
+++ js/index.js
@@ -0,0 +1,15 @@
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.5.1/katex.min.css">/**
+ * Main JS file for Casper behaviours
+ */
+
+/*globals jQuery, document */
+(function ($) {
+    "use strict";
+
+    $(document).ready(function(){
+
+        $(".post-content").fitVids();
+
+    });
+
+}(jQuery));
