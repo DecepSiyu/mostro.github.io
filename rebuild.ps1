c25d602ff49da3d6f311551d60649c11bacafd52
Mostro <616040809@qq.com>
2018-02-04 23:44:36 +0800

    添加适用于windows的脚本

---
+++ rebuild.ps1
@@ -0,0 +1,12 @@
+npm install hexo --save
+npm install hexo-server --save
+npm install hexo-admin --save
+npm install hexo-generator-archive --save
+npm install hexo-generator-tag --save
+npm install hexo-deployer-git --save
+npm install hexo-generator-sitemap --save
+npm install hexo-generator-category --save
+npm install hexo-generator-index --save
+npm install hexo-tag-katex --save
+
+npm install
